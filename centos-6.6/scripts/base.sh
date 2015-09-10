5aa24e598588879ea2cf568c5e289d006bff6452
Limech <limech@gmail.com>
2015-09-10 00:06:39 -0400

    Installing Puppet agent

--- centos-6.6/scripts/base.sh
+++ centos-6.6/scripts/base.sh
@@ -3,3 +3,9 @@
 set -e
 set -x
 
+rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
+yum install -y puppet
+
+#mkdir /home/vagrant/puppet-stage/
+#chmod 777 /home/vagrant/puppet-stage/
+chmod 755 /home/vagrant/cis-cat-full/CIS-CAT.sh
