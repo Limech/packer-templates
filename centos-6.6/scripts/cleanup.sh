10188b49767c5ee48236c80975777f366ace7366
Limech <limech@gmail.com>
2015-09-18 14:44:18 -0400

    Installation of ClamAV antivirus

--- centos-6.6/scripts/cleanup.sh
+++ centos-6.6/scripts/cleanup.sh
@@ -1,5 +1,8 @@
 #yum -y erase gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
-#yum -y clean all
+yum -y clean all
 rm -rf VBoxGuestAdditions_*.iso
 rm -rf /tmp/rubygems-*
-
+rm -rf /home/vagrant/puppet-rpm/*
+rmdir /home/vagrant/puppet-rpm/
+rm -rf /home/vagrant/clamav/*
+rmdir /home/vagrant/clamav/
