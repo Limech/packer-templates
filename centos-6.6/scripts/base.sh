1f97e2a7ec9d2360456c9d6a5fbf8310a9b1b7b3
Limech <limech@gmail.com>
2015-09-23 09:16:53 -0400

    Run CIS-CAT audit as part of provisioning

--- centos-6.6/scripts/base.sh
+++ centos-6.6/scripts/base.sh
@@ -3,17 +3,19 @@
 set -e
 set -x
 
+# Install Puppet for CIS-CAT provisioning
 rpm -ivh /home/vagrant/puppet-rpm/*.rpm
- 
-#mkdir /home/vagrant/puppet-stage/
-#chmod 777 /home/vagrant/puppet-stage/
+
+# Ensure CIS-CAT audit script can be executed
 chmod 755 /home/vagrant/cis-cat-full/CIS-CAT.sh
 
+# Remove default repositories to prevent online updates during provisioning.
 yum-config-manager --disable base
 yum-config-manager --disable extras
 yum-config-manager --disable updates
 yum clean all
 
+# CIS 1.2.1
 rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6
 
 # CIS 3.4
@@ -25,3 +27,12 @@ echo "/tmp /var/tmp none bind 0 0" >> /etc/fstab
 #CIS 6.3.1 This causes /etc/libuser.conf to also be updated as doing it in kickstart file
 # only updated /etc/login.defs
 authconfig --enableshadow --passalgo=sha512 --update
+
+#CIS 1.1.17
+mkdir /vagrant
+chmod +t /vagrant
+chmod +t -R /home/vagrant/
+
+
+
+
--- centos-6.6/scripts/cleanup.sh
+++ centos-6.6/scripts/cleanup.sh
@@ -1,8 +1,32 @@
-#yum -y erase gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
+#!/bin/bash
+
+# CIS 5.1.4 For some reason, this one doesn't stick with Puppet.. trying to fix it here.
+chmod 0600 /var/log/boot.log
+
+set -e
+set -x
+
+
+## Delete stuff we no longer need
+
 yum -y clean all
+
 rm -rf VBoxGuestAdditions_*.iso
 rm -rf /tmp/rubygems-*
+
 rm -rf /home/vagrant/puppet-rpm/*
 rmdir /home/vagrant/puppet-rpm/
+
 rm -rf /home/vagrant/clamav/*
 rmdir /home/vagrant/clamav/
+
+chmod -R +t /tmp/packer-puppet-masterless/module-0/cis
+
+# Restart network in preparation for running CIS-CAT audit
+sysctl -p
+
+
+
+
+
+
